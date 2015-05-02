99dd9be1e340557b95b1e8dd924873391ad1578c
root <edward>
2015-05-02 16:48:28 -0700

    first draft ofscript

---
+++ basic/config.sh
@@ -0,0 +1,11 @@
+export PROFILE=awsdevadmin
+export REGION=us-east-1
+export CLUSTER_NAME=SparkCluster
+export AMI_VSN=3.7
+export INSTANCE_TYPE=m3.xlarge 
+export INSTANCE_COUNT=3
+export LOG_URL=s3://awssparkec2
+export DEBUG=true
+export KEYNAME=coreostrain 
+
+
